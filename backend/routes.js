12614f03416d06ea7d742d003b35098f3c15f07c
nyxNyschd <nyxnyschd@posteo.de>
2022-09-18 11:08:30 +0200

    added backend

---
+++ backend/routes.js
@@ -0,0 +1,74 @@
+const express = require('express'); 
+const router = express.Router();
+const Post = require('./models/posts');
+
+
+ // GET all posts
+ router.get('/posts', async(req, res) => {
+    const allPosts = await Post.find();
+    console.log(allPosts);
+    res.send(allPosts);
+}); 
+ 
+// POST one post 
+router.post('/posts', async(req, res) => { 
+    const newPost = new Post({ 
+        title: req.body.title, 
+        location: req.body.location, 
+        image_id: req.body.image_id 
+    }) 
+    await newPost.save(); 
+    res.send(newPost); 
+});  
+
+// POST one post via id
+router.get('/posts/:id', async(req, res) => {
+    try {
+        const post = await Post.findOne({ _id: req.params.id });
+        console.log(req.params);
+        res.send(post);
+    } catch {
+        res.status(404);
+        res.send({
+            error: "Post does not exist!"
+        });
+    }
+});
+
+// PATCH (update) one post
+router.patch('/posts/:id', async(req, res) => {
+    try {
+        const post = await Post.findOne({ _id: req.params.id })
+
+        if (req.body.title) {
+            post.title = req.body.title
+        }
+
+        if (req.body.location) {
+            post.location = req.body.location
+        }
+
+        if (req.body.image_id) {
+            post.image_id = req.body.image_id
+        }
+
+        await Post.updateOne({ _id: req.params.id }, post);
+        res.send(post)
+    } catch {
+        res.status(404)
+        res.send({ error: "Post does not exist!" })
+    }
+});
+
+// DELETE one post via id
+router.delete('/posts/:id', async(req, res) => {
+    try {
+        await Post.deleteOne({ _id: req.params.id })
+        res.status(204).send()
+    } catch {
+        res.status(404)
+        res.send({ error: "Post does not exist!" })
+    }
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express'); 
+const cors = require('cors');
+const routes = require('./routes');
+//const postsRoutes = require('./middleware/routes/posts.routes');
+//const uploadRoutes = require('./middleware/routes/upload.routes');
+const mongoose = require('mongoose'); 
+
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
+//app.use('/posts', postsRoutes);
+//app.use('/img', uploadRoutes);
+
+app.listen(PORT, (error) =>{
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`server running on http://localhost:${PORT}`);
+    }
+});
+
+// connect to mongoDB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('connected to DB');
+});  
