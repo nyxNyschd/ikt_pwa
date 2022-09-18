453eaa7e6491f37042151b6fe758b1c8297cc64c
nyxNyschd <nyxnyschd@posteo.de>
2022-09-18 18:33:20 +0200

    changed filename in download.routes

--- backend/routes/download.routes.js
+++ backend/routes/download.routes.js
@@ -4,6 +4,7 @@ const Grid = require("gridfs-stream");
 const router = express.Router();
 const conn = require('../.conn');
 url = conn.connectionString;
+var fs = require('fs');
 
 const connect = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
 let gfs, gfsb;
@@ -16,9 +17,11 @@ connect.once('open', () => {
     gfs = Grid(connect.db, mongoose.mongo);
 });
 
-router.get('/downloads/:filename', async(req, res) => {
+
+router.get('/download/:filename', async(req, res) => {
+    
     try {
-        const cursor = await gfs.collection('uploads').find({ filename: req.params.originalname });
+        const cursor = await gfs.collection('uploads').find({ filename: req.params.filename });
         cursor.forEach(doc => {
             console.log('doc', doc);
             gfsb.openDownloadStream(doc._id).pipe(res);
@@ -28,5 +31,19 @@ router.get('/downloads/:filename', async(req, res) => {
         res.send("not found");
     }
 });
+
+/* gfsb.openDownloadStreamByName(filename).
+    pipe(fs.createWriteStream('./'+filename)).
+        on('error', function (error) {
+            console.log("error" + error);
+            res.status(404).json({
+                msg: error.message
+            });
+        }).
+        on('finish', function () {
+            console.log('done!');
+            res.send('Downloaded successfully!')
+});
+ */
 
 module.exports = router;
